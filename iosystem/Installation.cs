648580bf4a09d859e2369b5aedb615df035d6dcf
rickcoker <rick.coker@iobjectsinc.com>
2015-10-01 05:45:56 -0500

    Initial commit

---
+++ iosystem/Installation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iosystem
+{
+    public class Installation
+    {
+        public const int SystemInstallKey = 1;
+    }
+}
---
+++ iosystem/UseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iosystem
+{
+    public class UseContext
+    {
+        public enum AppKey
+        {
+            Webapp = 1,
+            Service = 2
+        }
+    }
+}
